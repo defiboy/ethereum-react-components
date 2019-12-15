f809f0936f92c4816453ba2ab02c6bb5abaa46dd
Solid Studio <solidstudio@labchain.io>
2019-12-15 20:55:38 +0000

    cfg minimal test

--- packages/evm/test/EVMStack.test.ts
+++ packages/evm/test/EVMStack.test.ts
@@ -7,50 +7,59 @@ describe('EVMStack', () => {
     stack = new EVMStack()
   })
 
-  it('test push', () => {
+  test('push', () => {
     stack.push(Word.createLiteral('80'))
     stack.push(Word.createLiteral('81'))
+
     expect(stack.length()).toEqual(2)
   })
 
-  it('Test pop', () => {
+  test('pop', () => {
     stack.push(Word.createLiteral('80'))
     stack.push(Word.createLiteral('81'))
+
     const pop = stack.pop()
+
     expect(pop).toEqual(Word.createLiteral('81'))
     expect(stack.length()).toEqual(1)
   })
 
-  it('Test peek', () => {
+  test('peek', () => {
     stack.push(Word.createLiteral('80'))
     stack.push(Word.createLiteral('81'))
+
     const pop = stack.peek()
+
     expect(pop).toEqual(Word.createLiteral('81'))
     expect(stack.length()).toEqual(2)
   })
 
-  it('Test get', () => {
+  test('get', () => {
     stack.push(Word.createLiteral('80'))
     stack.push(Word.createLiteral('81'))
     stack.push(Word.createLiteral('82'))
+
     const get0 = stack.get(0)
     const get1 = stack.get(1)
     const get2 = stack.get(2)
+
     expect(get0).toEqual(Word.createLiteral('82'))
     expect(get1).toEqual(Word.createLiteral('81'))
     expect(get2).toEqual(Word.createLiteral('80'))
     expect(stack.length()).toEqual(3)
   })
 
-  it('Test put', () => {
+  test('put', () => {
     stack.push(Word.createLiteral('01'))
     stack.push(Word.createLiteral('02'))
     stack.push(Word.createLiteral('03'))
     stack.put(2, Word.createLiteral('99'))
     stack.put(0, Word.createLiteral('ff'))
+
     const top0 = stack.pop()
     const top1 = stack.pop()
     const top2 = stack.pop()
+
     expect(top0).toEqual(Word.createLiteral('ff'))
     expect(top1).toEqual(Word.createLiteral('02'))
     expect(top2).toEqual(Word.createLiteral('99'))
