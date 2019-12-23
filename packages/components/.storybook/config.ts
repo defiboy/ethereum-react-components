0cfe4c6d9927ee7fc2648f5dddb1ecae5b9a6312
Dotiqal <dotiqal@gmail.com>
2019-12-23 23:23:36 +0100

    some render refact. added inputs from knowbs in storybook

--- packages/components/.storybook/config.ts
+++ packages/components/.storybook/config.ts
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/react';
-
+import { setConsoleOptions } from '@storybook/addon-console';
+import { addDecorator } from '@storybook/react';
+import { withConsole } from '@storybook/addon-console';
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.tsx$/), module);
+
+setConsoleOptions({
+  panelExclude: []
+});
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
