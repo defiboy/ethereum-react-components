3eaa01efa9fb303aa7993162092ba7dd54aca306
Solid Studio <solidstudio@labchain.io>
2020-02-02 12:50:25 +0000

    fixed tests

--- packages/utilities/test/disassembler/EVMDisassembler.test.ts
+++ packages/utilities/test/disassembler/EVMDisassembler.test.ts
@@ -69,8 +69,8 @@ describe('Disassembler test', () => {
   it('Test disassemble contract with version 5.3', () => {
     const bytecode = '608060405234801561001057600080fd5b506103e86000819055506107d0600181905550610152806100326000396000f3fe608060405234801561001057600080fd5b5060043610610069576000357c0100000000000000000000000000000000000000000000000000000000900480633fa4f2451461006e578063552410771461008c578063d6ab10b4146100ba578063f9f16ef2146100e8575b600080fd5b610076610106565b6040518082815260200191505060405180910390f35b6100b8600480360360208110156100a257600080fd5b810190808035906020019092919050505061010c565b005b6100e6600480360360208110156100d057600080fd5b8101908080359060200190929190505050610116565b005b6100f0610120565b6040518082815260200191505060405180910390f35b60005481565b8060008190555050565b8060018190555050565b6001548156fea165627a7a723058209acb4546d7af997cefc4763e910bdfbacf42dc35015954b1bf8b24ad557b16370029'
     const contract: DisassembledContract = disassemblerInstance.disassembleContract(bytecode, CompilerVersion.SOLIDITY_5)
-    const constructor = contract.constructor
-    const runtime = contract.runtime
+    const constructor = contract.constructorOperations
+    const runtime = contract.runtimeOperations
     const firstConstructor = constructor[0]
     const lastConstructor = constructor[constructor.length - 1]
     const firstRuntime = runtime[0]
@@ -110,7 +110,7 @@ describe('Disassembler test', () => {
     const contract: DisassembledContract = disassemblerInstance.disassembleContract(bytecode)
 
     expect(contract.hasConstructor).toBeFalsy()
-    expect(contract.constructor.length).toEqual(0)
+    expect(contract.constructorOperations.length).toEqual(0)
     expect(contract.bytecode).toEqual(bytecode)
   })
 
