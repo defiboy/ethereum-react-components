2a0bbe79d0961912652e2936b1e6a47fe772d516
Edson Alcala <edson_alcala@msn.com>
2021-10-12 14:03:30 +0100

    updated compiler version

--- packages/types/src/CompilerVersion.ts
+++ packages/types/src/CompilerVersion.ts
@@ -1,5 +1,7 @@
 export enum CompilerVersion {
   SOLIDITY_4 = "SOLIDITY_4",
   SOLIDITY_5 = "SOLIDITY_5",
-  SOLIDITY_6 = "SOLIDITY_4"
+  SOLIDITY_6 = "SOLIDITY_6",
+  SOLIDITY_7 = "SOLIDITY_7",
+  SOLIDITY_8 = "SOLIDITY_8",
 }
